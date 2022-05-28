b383c182bc90afb18511631f247217d9a3b243ec
phamduchongan93 <vincentpham1993@gmail.com>
2022-05-27 17:14:06 -0700

    continue cleaning

--- data/file_fix.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# fix /bin/bash^M: bad interpreter: No such file or directory bug
-# more details: https://askubuntu.com/questions/304999/not-able-to-execute-a-sh-file-bin-bashm-bad-interpreter
-
-sed -i -e 's/\r$//' "$1" #fixing the conflict 
